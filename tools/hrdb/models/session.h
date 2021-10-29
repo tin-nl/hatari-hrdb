107035bbac5b27310e627de4378f1e649ebd96f4
Steven Tattersall <tattlemuss@github.com>
2021-10-29 12:28:09 +0100

    Bump version string

--- tools/hrdb/models/session.h
+++ tools/hrdb/models/session.h
@@ -10,7 +10,7 @@ class QTemporaryFile;
 class Dispatcher;
 class TargetModel;
 
-#define VERSION_STRING      "0.003 (06 Sept 2021)"
+#define VERSION_STRING      "0.004 (29 Oct 2021)"
 
 // Shared runtime data about the debugging session used by multiple UI components
 // This data isn't persisted over runs (that is saved in Settings)
